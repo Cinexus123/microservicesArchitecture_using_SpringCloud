2d43ba95b2a6ee290f547535ef5142e28153ec08
Marcin Niełacny <you@example.com>
2021-01-18 15:18:02 +0100

    Added instanceId for PaymentApi

--- payment-service/src/main/java/com/example/payment_service/PaymentApi.java
+++ payment-service/src/main/java/com/example/payment_service/PaymentApi.java
@@ -1,13 +1,17 @@
 package com.example.payment_service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PaymentApi {
 
+    @Value("${eureka.instance.instance-id}")
+    private String instanceId;
+
     @GetMapping
     public String hello(){
-        return "Hello world from PaymentApi";
+        return "Hello world from PaymentApi" + instanceId;
     }
 }
