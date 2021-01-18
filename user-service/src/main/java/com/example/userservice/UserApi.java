b41cb9856383b7fcc94019cee5f8f76263914a74
Marcin Niełacny <you@example.com>
2021-01-18 16:46:29 +0100

    Added config server and configuration to pull information

--- user-service/src/main/java/com/example/userservice/UserApi.java
+++ user-service/src/main/java/com/example/userservice/UserApi.java
@@ -1,13 +1,19 @@
 package com.example.userservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserApi {
 
+
+    @Value("${app.user}")
+    private String appUser;
+
+
     @GetMapping
     public String hello(){
-        return "Hello world from UserApi";
+        return "Hello world from UserApi" + appUser;
     }
 }
